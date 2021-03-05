963851319aa6dbbd6e3e83ab97f6cbc0f124d46c
Buks <abolarinbukolastella@gmail.com>
2021-03-05 20:23:54 +0100

    done

--- script.js
+++ script.js
@@ -8,18 +8,19 @@ const selectOpt = document.querySelector(".select_opt");
 const first = document.querySelector(".first");
 const countries = document.querySelector(".countries");
 const select = document.querySelector(".select");
-const count = document.querySelectorAll(".count_container");
+const selectToggle = document.querySelector(".select_toggle");
 const arrow = document.querySelector(".arrow");
 const back = document.querySelector(".back");
 const second = document.querySelector(".second");
 const body = document.querySelector("body");
-const a = document.querySelector(".end_render").querySelectorAll("a");
 
 const color = [head, form, text, selectMain, selectOpt, back, second];
 const backgroundColor = [countries, first, select, second, body];
 
 //dark mode
 dark.addEventListener("click", function () {
+  const count = document.querySelectorAll(".count_container");
+  const a = document.querySelector(".end_render").querySelectorAll("a");
   color.forEach((ev) => {
     ev.classList.toggle("dark_mode");
   });
@@ -30,20 +31,35 @@ dark.addEventListener("click", function () {
   });
   a.forEach((ev) => ev.classList.toggle("dark_mode"));
 });
-arrow.addEventListener("click", function () {
+//select
+const selectDisplay = function () {
   selectOpt.classList.toggle("hidden");
-});
+};
+arrow.addEventListener("click", selectDisplay);
 
+//render error
+const renderError = function (element) {
+  const html = `<div class='error'> Not Available, Try again! </div>`;
+  element.innerHTML = "";
+  element.insertAdjacentHTML("afterbegin", html);
+};
+const spinner = function (element) {
+  const html = `<div class='spinner'> <i class="fas fa-spinner fa-7x"></i> </div>`;
+  element.innerHTML = "";
+  element.insertAdjacentHTML("afterbegin", html);
+};
 //render all countries
-const renderAll = async function () {
+const renderAll = async function (addr) {
   try {
-    const res = await fetch("https://restcountries.eu/rest/v2/all");
+    spinner(countries);
+    const res = await fetch(addr);
     const data = await res.json();
-    console.log(data);
     const html = data
       .map(
         (ev) => ` 
-      <div class="count_container">
+      <div class="count_container ${
+        head.classList.contains("dark_mode") ? "dark_mode" : ""
+      }">
         <div class="container_top">
           <img src=${ev.flag} alt="flag" />
         </div>
@@ -56,9 +72,115 @@ const renderAll = async function () {
       </div>`
       )
       .join("");
+    countries.innerHTML = "";
     countries.insertAdjacentHTML("afterbegin", html);
   } catch (error) {
     console.log(error);
+    renderError(countries);
   }
 };
-renderAll();
+renderAll("https://restcountries.eu/rest/v2/all");
+
+//search for countries
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (!text.value) return;
+  renderAll(`https://restcountries.eu/rest/v2/name/${text.value}`);
+  text.value = "";
+});
+//filter by region
+select.addEventListener("click", function (e) {
+  if (!e.target.closest("li")) return;
+  const data = e.target.textContent;
+  selectDisplay();
+  if (data == "All") {
+    selectToggle.textContent = "Filter by Region";
+    renderAll("https://restcountries.eu/rest/v2/all");
+  } else {
+    selectToggle.textContent = data;
+    renderAll(`https://restcountries.eu/rest/v2/region/${data}`);
+  }
+});
+//detailed page
+const renderDetail = async function (addr) {
+  try {
+    spinner(second);
+    const res = await fetch(addr);
+    let data = await res.json();
+    if (!data.length) {
+      const foo = [];
+      foo.push(data);
+      data = foo;
+    }
+    const html = data
+      .map(
+        (ev) => ` 
+        <div class="second_img">
+        <img src=${ev.flag} alt="flag" />
+      </div>
+      <div class="right">
+        <h1>${ev.name}</h1>
+        <div class="right_content">
+          <ul>
+            <li>Native Name: <span>${ev.nativeName}</span></li>
+            <li>population: <span>${ev.population.toLocaleString()}</span></li>
+            <li>region: <span>${ev.region}</span></li>
+            <li>sub region: <span>${ev.subregion}</span></li>
+            <li>capital: <span>${ev.capital}</span></li>
+          </ul>
+          <ul>
+            <li>top level domain <span>${ev.topLevelDomain[0]}</span></li>
+            <li>currencies <span>${ev.currencies[0].name}</span></li>
+            <li>languages <span>${ev.languages
+              .map((ev) => ev.name)
+              .join(",")}</span></li>
+          </ul>
+        </div>
+        <div class="right_end ${ev.borders.length == 0 ? "hidden" : ""}">
+          <span> border countries: </span>
+          <div class="end_render">${ev.borders
+            .map(
+              (ev) =>
+                `<a class='${
+                  head.classList.contains("dark_mode") ? "dark_mode" : ""
+                }'>${ev}</a>`
+            )
+            .join("")}
+          </div>
+        </div>
+      </div>
+     `
+      )
+      .join("");
+    second.innerHTML = "";
+    second.insertAdjacentHTML("afterbegin", html);
+  } catch (error) {
+    console.error(error);
+    renderError(second);
+  }
+};
+const display = [form, select, countries, back, second];
+let border;
+countries.addEventListener("click", function (e) {
+  //
+  display.forEach((ev) => ev.classList.toggle("hidden"));
+  border = document.querySelector(".end_render");
+  if (!e.target.closest(".count_container")) return;
+  const data = e.target.closest(".count_container").querySelector("h3")
+    .textContent;
+  renderDetail(`https://restcountries.eu/rest/v2/name/${data}`);
+});
+//border detail page
+second.addEventListener("click", function (e) {
+  if (!e.target.closest("a")) return;
+  const data = e.target.closest("a").textContent;
+  renderDetail(`https://restcountries.eu/rest/v2/alpha/${data}`);
+});
+
+//back
+back.addEventListener("click", function () {
+  display.forEach((ev) => ev.classList.toggle("hidden"));
+  selectToggle.textContent = "Filter by Region";
+  renderAll("https://restcountries.eu/rest/v2/all");
+});
